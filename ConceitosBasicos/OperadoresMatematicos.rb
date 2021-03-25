419ab3fac586e4665ff0405d060569a1feee8ade
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-03-25 03:53:04 -0300

    Operadores Matemáticos - Atualização

--- ConceitosBasicos/OperadoresMatematicos.rb
+++ ConceitosBasicos/OperadoresMatematicos.rb
@@ -19,3 +19,14 @@ puts a / b # divisão
 puts a % b # resto da divisão entre os valores
 puts a ** b # potência entre os valores
 
+
+puts "Digite um valor"
+a = gets.chomp.to_i 
+puts "Digite outro valor"
+b = gets.chomp.to_i
+puts "Calculando a soma entre #{a} e #{b}"
+puts "O resultado é #{a + b}"
+
+
+
+
