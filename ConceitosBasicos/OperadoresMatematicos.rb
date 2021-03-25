021a5850e9600bdbd8272d692dc377717e9b0af4
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-03-25 03:33:06 -0300

    Operadores Matemáticos

---
+++ ConceitosBasicos/OperadoresMatematicos.rb
@@ -0,0 +1,9 @@
+a = 7
+b = 5
+
+puts a + b # adição
+puts a - b # subtração
+puts a * b # multiplicação
+puts a / b # divisão
+puts a % b # resto da divisão entre os valores
+puts a ** b # potência entre os valores
