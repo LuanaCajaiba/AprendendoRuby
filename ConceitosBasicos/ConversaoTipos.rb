1f26ff7e374bff153dcdf3b5f0b6912c9beb6861
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-03-25 03:11:21 -0300

    Atualização

---
+++ ConceitosBasicos/ConversaoTipos.rb
@@ -0,0 +1,11 @@
+a = "10"
+a.to_i #to_i = converte valor para um valor inteiro
+puts a
+
+b = "54.7"
+b.to_f #to_f = converte valor para um valor float
+puts b
+
+c = 300
+c.to_s #to_s = converte o valor para uma string
+puts c
--- introducao/helloworld.rb
+++ introducao/helloworld.rb
@@ -2,3 +2,8 @@ puts "Olá, mundo!"
 
 # com o comando irb é possível ter um ambiente de teste de código
 # exit - para sair desse ambiente
+
+=begin
+puts = com quebra de linhas
+put = sem quebra de linha
+=end
