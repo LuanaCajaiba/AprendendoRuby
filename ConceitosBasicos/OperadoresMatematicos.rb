dc14d02d883306ed2fc87d8f71a38d24eac9c0c3
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-03-25 03:39:44 -0300

    Operadores Matemáticos - Atualização

--- ConceitosBasicos/OperadoresMatematicos.rb
+++ ConceitosBasicos/OperadoresMatematicos.rb
@@ -7,3 +7,15 @@ puts a * b # multiplicação
 puts a / b # divisão
 puts a % b # resto da divisão entre os valores
 puts a ** b # potência entre os valores
+
+puts "Digite um valor"
+a = gets.chomp.to_i #é necessário converter a inteiro, pois toda saída é string | chomp irá remover caracteres como o de nova linha
+puts "Digite outro valor"
+b = gets.chomp.to_i
+puts a + b # adição
+puts a - b # subtração
+puts a * b # multiplicação
+puts a / b # divisão
+puts a % b # resto da divisão entre os valores
+puts a ** b # potência entre os valores
+
