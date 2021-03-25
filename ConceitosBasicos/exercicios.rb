d57d8113a8bfa67d16deb2fffc45375c4a60f9d2
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-03-25 20:18:04 -0300

    Exercícios - Parte1

---
+++ ConceitosBasicos/exercicios.rb
@@ -0,0 +1,31 @@
+#Questão 1.1 - Crie um script em Ruby que leia um número inteiro e mostre seu sucessor.
+=begin
+print "Insisra um número: "
+a = gets.chomp.to_i 
+puts "O sucessor desse número é #{a + 1}"
+
+#Questão 1.2 Crie um script em Ruby que leia um número inteiro e mostre seu antecessor.
+
+print "Insira um número: "
+a = gets.chomp.to_i
+puts "O antecessor desse número é #{a - 1}"
+
+#Questão 1.3 Crie um script em Ruby que leia dois números inteiros e mostre a soma dos dois.
+
+a = gets.chomp.to_i
+b = gets.chomp.to_i
+x = "A soma dos valores inseridos é: #{a + b}"
+puts x
+
+
+
+#Questão 1.4 Crie um script em Ruby que leia um número inteiro e mostre o dobro desse número.
+
+a = gets.chomp.to_i
+puts "O dobro desse número é: #{a * 2}"
+=end
+#Questão 1.5 Crie um script em Ruby que leia um número inteiro e mostre a metade desse número.
+
+a = gets.chomp.to_f
+puts "A metade desse número é: #{a / 2}"
+
